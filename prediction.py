d73bd138912fb5d3c0828bd49150a932d429c7ec
root <root@ip-172-31-11-177.ap-south-1.compute.internal>
2019-06-15 09:14:20 +0000

    unnecessary comments removed

--- prediction.py
+++ prediction.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 from sklearn import metrics
@@ -13,17 +9,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 
-# In[3]:
-
-
 df = pd.read_csv('I:\ML_ProjectAdhoc\Shashank_ML\Heart-Disease-ML-Project\heart.csv')
 df.head()
 
 
 # # Decision Tree classifier
-
-# In[4]:
-
 
 from sklearn.tree import DecisionTreeClassifier
 features = df[['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal']]
@@ -40,35 +30,13 @@ print("Decision Tree Accuracy:- ",metrics.accuracy_score(y_test,predict))
 
 
 # # random forest classifier
-# 
-
-# In[5]:
-
 
 clf = RandomForestClassifier(criterion='entropy',n_estimators=7)
 
-
-# In[6]:
-
-
 trained = clf.fit(x_train,y_train)
-
-
-# In[7]:
-
 
 predicted = trained.predict(x_test)
 predicted
 
-
-# In[8]:
-
-
 print("Accuracy from random forest:- ",metrics.accuracy_score(y_test,predicted)*100)
-
-
-# In[ ]:
-
-
-
 
